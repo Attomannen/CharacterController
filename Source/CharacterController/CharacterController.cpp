7692df6eaaad04179eeea2615c3b62a42723dc7b
Anton Broman <antobrom@playgroundsquad.com>
2024-08-23 10:17:35 +0200

    Removed old project

--- Source/CharacterController/CharacterController.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "CharacterController.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CharacterController, "CharacterController" );
